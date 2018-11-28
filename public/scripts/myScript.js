b5cfc3c43fa59206fcccff684d209dba1c28bb34
Irakli1121 <i_iremasshvili@cu.edu.ge>
2018-11-28 23:54:46 +0400

    cleared verification code

--- public/scripts/myScript.js
+++ public/scripts/myScript.js
@@ -1,25 +1,21 @@
-console.log("script working");
-
-
 let showButton = document.getElementById("showHidden");
 let submitDiv = document.getElementById("submitDiv");
 
 showButton.addEventListener('click', ()=>{
-    console.log("clicked button");
-
-    if (submitDiv.style.display === "none") {
-        submitDiv.style.display = "block";
-    } else {
-        submitDiv.style.display = "none";
-    }
+    changeVisibility();
 });
 
 let noButton = document.getElementById("hideAgain");
 
 noButton.addEventListener('click', () => {
+    changeVisibility();
+});
+
+
+function changeVisibility(){
     if (submitDiv.style.display === "none") {
         submitDiv.style.display = "block";
     } else {
         submitDiv.style.display = "none";
     }
-});
+}
